814e9e8eee49832fdbeb1f9462af033be244e015
Jane Su <sujievic@gmail.com>
2021-12-27 18:24:24 +1100

    sorted square

---
+++ array/977 Squares of a Sorted Array.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        square = [0] * len(nums)
+        i = len(nums) - 1
+        right_square = int(math.pow(nums[right], 2))
+        left_square = int(math.pow(nums[left], 2))
+        while left < right:
+            if left_square <= right_square:
+                square[i] = right_square
+                right -= 1
+                right_square = int(math.pow(nums[right], 2))
+            else:
+                square[i] = left_square
+                left += 1
+                left_square = int(math.pow(nums[left], 2))
+            i -= 1
+        square[i] = int(math.pow(nums[left], 2))
+        return square
